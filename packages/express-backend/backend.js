506e63d7667b5c8ca2e9d9bfc079dc115cd4e754
Colby Watts <colby.watts59@yahoo.com>
2024-04-14 23:42:32 -0700

    Added fetching users by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,21 +33,17 @@ const users = {
   ]
 };
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
